28a36782a3aabef072aafadba630fe0e48accd6e
naveed.shoukat1 <naveed.shoukat@invozone.com>
2023-07-04 21:56:47 +0500

    test case 10, 11, 12, 13, and 14

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -17,6 +17,12 @@ describe('initiates', () => {
   let city = fakerEN_US.location.city()
   let zipcode = fakerEN_US.location.zipCode()
   let phonenumber = faker.phone.number('+92 ### ### ## ##')
+  let cardholder = faker.finance.accountName()
+  let cardnumber = faker.finance.creditCardNumber({ issuer: 'visa' })
+  let cvc = faker.finance.creditCardCVV()
+  // let expiryYear = faker.date.between().
+  // let expiryMonth = faker.date.month({ context: true })
+
 
 
   it('Test Case 1: Register User', () => {
@@ -105,7 +111,7 @@ describe('initiates', () => {
     cy.get('.product-information > :nth-child(8)').should('contain','Brand:').should('contain',' Polo')
   });
 
-  it.only('Test Case 9: Search Product', () => {
+  it('Test Case 9: Search Product', () => {
     cy.contains('Products').click()
     cy.url().should('contain', '/products')
     cy.contains('All Products')
@@ -113,7 +119,112 @@ describe('initiates', () => {
     cy.get('#submit_search').click()
     cy.url().should('contain', '/products?search=Men')
     cy.contains('Searched Products')
+  });
 
+  it('Test Case 10: Verify Subscription in home page', () => {
+    cy.scrollTo('bottom')
+    cy.get('#susbscribe_email').clear().type(email)
+    cy.get('#subscribe').click()
+    cy.contains('You have been successfully subscribed!').should('be.visible')
+  });
+
+  it('Test Case 11: Verify Subscription in Cart page', () => {
+    cy.get('.shop-menu > .nav > :nth-child(3) > a').should('contain',' Cart').click()
+    cy.get('#susbscribe_email').clear().type(email)
+    cy.get('#subscribe').click()
+    cy.contains('You have been successfully subscribed!').should('be.visible')
+  });
+
+  it('Test Case 12: Add Products in Cart', () => {
+    cy.contains('Products').click()
+    cy.url().should('contain', '/products')
+    cy.get('.left-sidebar > :nth-child(1)').should('contain','Category')
+    cy.get(':nth-child(3) > .product-image-wrapper > .single-products > .productinfo > img').trigger('mouseover')
+    cy.contains('Add to cart').click()
+    cy.get('.modal-footer > .btn').should('contain','Continue Shopping').click()
+    cy.get(':nth-child(4) > .product-image-wrapper > .single-products > .productinfo > img').trigger('mouseover')
+    cy.contains('Add to cart').click()
+    cy.contains('View Cart').click()
+    cy.get('.price').should('contain','Price')
+    cy.get('.quantity').should('contain','Quantity')
+    cy.get('.total').should('contain','Total')
+  });
+
+  it('Test Case 13: Verify Product quantity in Cart ', () => {
+    cy.contains('Products').click()
+    cy.url().should('contain', '/products')
+    cy.get('.left-sidebar > :nth-child(1)').should('contain','Category')
+    cy.get(':nth-child(3) > .product-image-wrapper > .choose > .nav > li > a').should('contain', 'View Product').click()
+    cy.url().should('contain', '/product_details/1')
+    cy.get('.product-information > h2').should('contain','Blue Top')
+    cy.get('.product-information > :nth-child(3)').should('contain','Category: Women > Tops')
+    cy.get(':nth-child(5) > span').should('contain','Rs. 500')
+    cy.get('.product-information > :nth-child(6)').should('contain','Availability:').should('contain',' In Stock')
+    cy.get('.product-information > :nth-child(7)').should('contain','Condition:')
+    cy.get('.product-information > :nth-child(8)').should('contain','Brand:').should('contain',' Polo')
+    cy.get('#quantity').clear().type('4')
+    cy.get(':nth-child(5) > .btn').should('contain','Add to cart').click()
+    cy.contains('View Cart').click()
+    cy.get('.quantity').should('contain','Quantity')
+    cy.get('.disabled').contains('4')
+  });
+
+  it.only('Test Case 14: Place Order: Register while Checkout', () => {
+    cy.contains('Products').click()
+    cy.url().should('contain', '/products')
+    cy.get('.left-sidebar > :nth-child(1)').should('contain','Category')
+    cy.get(':nth-child(3) > .product-image-wrapper > .choose > .nav > li > a').should('contain', 'View Product').click()
+    cy.url().should('contain', '/product_details/1')
+    cy.get('.product-information > h2').should('contain','Blue Top')
+    cy.get('.product-information > :nth-child(3)').should('contain','Category: Women > Tops')
+    cy.get(':nth-child(5) > span').should('contain','Rs. 500')
+    cy.get('.product-information > :nth-child(6)').should('contain','Availability:').should('contain',' In Stock')
+    cy.get('.product-information > :nth-child(7)').should('contain','Condition:')
+    cy.get('.product-information > :nth-child(8)').should('contain','Brand:').should('contain',' Polo')
+    cy.get('#quantity').clear().type('4')
+    cy.get(':nth-child(5) > .btn').should('contain','Add to cart').click()
+    cy.contains('View Cart').click()
+    cy.get('.quantity').should('contain','Quantity')
+    cy.get('.disabled').contains('4')
+    cy.get('.col-sm-6 > .btn').should('contain' , 'Proceed To Checkout').click()
+    cy.get('.modal-body > :nth-child(2) > a > u').should('contain','Register / Login').click()
+    cy.get('input[name="name"]').should('be.visible').clear().type(fullname)
+    cy.get('[data-qa="signup-email"]').should('be.visible').clear().type(email)
+    cy.get('[data-qa="signup-button"]').should('contain','Signup').click()
+    cy.url().should('contain', 'https://www.automationexercise.com/signup')
+    cy.get('#id_gender1').should('be.visible').should('not.be.checked').check()
+    cy.get('[data-qa="password"]').type(password)
+    cy.get('[data-qa="days"]').should('contain', 'Day','have.value','Day').select('1')
+    cy.get('[data-qa="months"]').should('contain', 'Month','have.value','Month').select('February')
+    cy.get('[data-qa="years"]').should('contain', 'Year','have.value','Year').select('2000')
+    cy.get('#newsletter').should('be.visible').should('not.be.checked').check()
+    cy.get('#optin').should('be.visible').should('not.be.checked').check()
+    cy.get('[data-qa="first_name"]').should('be.visible').clear().type(firstname)
+    cy.get('[data-qa="last_name"]').should('be.visible').clear().type(Lastname)
+    cy.get('[data-qa="company"]').should('be.visible').clear().type(company)
+    cy.get('[data-qa="address"]').should('be.visible').clear().type(streetaddress)
+    cy.get('[data-qa="address2"]').should('be.visible').clear().type(streetaddress)
+    cy.get('[data-qa="country"]').should('contain', 'India','have.value','India').select('United States')
+    cy.get('[data-qa="state"]').should('be.visible').clear().type(state)
+    cy.get('[data-qa="city"]').should('be.visible').clear().type(city)
+    cy.get('[data-qa="zipcode"]').should('be.visible').clear().type(zipcode)
+    cy.get('[data-qa="mobile_number"]').should('be.visible').clear().type(phonenumber)
+    cy.get('[data-qa="create-account"]').should('be.enabled').click()
+    cy.url().should('contain', 'https://www.automationexercise.com/account_created')
+    cy.get('b').should('be.visible').should('contain','Account Created!')
+    cy.get('[data-qa="continue-button"]').click()
+    cy.get(':nth-child(10) > a').should('contain', fullname)
+    cy.get('.shop-menu > .nav > :nth-child(3) > a').should('contain', 'Cart').click()
+    cy.get('.col-sm-6 > .btn').should('contain', 'Proceed To Checkout').click()
+    cy.get('.form-control').clear().type(faker.lorem.lines(5))
+    cy.get(':nth-child(7) > .btn').should('contain', 'Place Order').click()
+    cy.get('[data-qa="name-on-card"]').clear().type(cardholder)
+    cy.get('[data-qa="card-number"]').clear().type(cardnumber)
+    cy.get('[data-qa="cvc"]').clear().type(cvc)
+    cy.get('[data-qa="expiry-month"]').clear().type('02')
+    cy.get('[data-qa="expiry-year"]').clear().type('2024')
+    cy.get('[data-qa="pay-button"]').should('contain', 'Pay and Confirm Order').click()
+    cy.contains('Order Placed!')
   });
 
 }) 
