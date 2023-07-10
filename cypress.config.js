cc5ddd2b1b7fcbe16d8532e66f408677afa8c459
naveed.shoukat1 <naveed.shoukat@invozone.com>
2023-07-10 17:10:47 +0500

    allure report configured

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,15 @@
 const { defineConfig } = require("cypress");
 const { isFileExist } = require('cy-verify-downloads');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on('task', { isFileExist })
+      allureWriter(on, config);
+            return config;
     },
   },
+  
 });
